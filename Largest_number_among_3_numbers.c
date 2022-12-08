b3cdbf0e5ac21ae4725be689dee99c842dd55935
shyamkumarbadiganti <119720218+shyamkumarbadiganti@users.noreply.github.com>
2022-12-08 20:46:12 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d%d",&a,&b,&c);     if(a>b&&a>b)     {         printf("%d",a);     }     else if(c>b&&a<c)     {         printf("%d",c);     }     else if(a<b&&c<b)     {         printf("%d",b);     } }

---
+++ Largest_number_among_3_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>b)
+    {
+        printf("%d",a);
+    }
+    else if(c>b&&a<c)
+    {
+        printf("%d",c);
+    }
+    else if(a<b&&c<b)
+    {
+        printf("%d",b);
+    }
+}
