55b8243b813cad25e36968fd99060784fefa6e76
shyamkumarbadiganti <119720218+shyamkumarbadiganti@users.noreply.github.com>
2023-01-20 11:23:32 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,i;     int c=0,k=0;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(a[i]<n)         {             c=1;         }         else         {             c=0;             break;         }     }     if(c==1)     printf("YES");     else     printf("NO"); }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i;
+    int c=0,k=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<n)
+        {
+            c=1;
+        }
+        else
+        {
+            c=0;
+            break;
+        }
+    }
+    if(c==1)
+    printf("YES");
+    else
+    printf("NO");
+}
