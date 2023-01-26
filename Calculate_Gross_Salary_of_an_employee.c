2b171e9410da410f28006f5834b5160e58748854
shyamkumarbadiganti <119720218+shyamkumarbadiganti@users.noreply.github.com>
2023-01-26 12:24:24 +0530

    #include<stdio.h> int main() { float g,p,d,h,b; scanf("%f%f%f",&b,&d,&h); p=(b)*0.12; g=b+p+d+h; printf("%.2f %.2f",p,g); }

--- Calculate_Gross_Salary_of_an_employee.c
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    float g,p,d,h,b;
-    scanf("%f%f%f",&b,&d,&h);
-    p=(b)*0.12;
-    g=b+p+d+h;
-    printf("%.2f
+float g,p,d,h,b;
+scanf("%f%f%f",&b,&d,&h);
+p=(b)*0.12;
+g=b+p+d+h;
+printf("%.2f
 %.2f",p,g);
 }
