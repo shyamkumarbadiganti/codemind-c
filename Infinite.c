643ca0a22b5b4d10d877710a4ce479ebc109d5c7
shyamkumarbadiganti <119720218+shyamkumarbadiganti@users.noreply.github.com>
2023-08-29 21:36:34 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,s;     while(n!=-1)     {         scanf("%d",&n);         s=pow(n,2);         if(n!=-1)         printf("%d ",s);
        }
        }

---
+++ Infinite.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,s;
+    while(n!=-1)
+    {
+        scanf("%d",&n);
+        s=pow(n,2);
+        if(n!=-1)
+        printf("%d
+",s);
+        
+    }
+    }
