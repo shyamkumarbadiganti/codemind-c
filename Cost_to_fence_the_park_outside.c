fd0315b342462ef86ea04eca9b161eb42487f999
shyamkumarbadiganti <119720218+shyamkumarbadiganti@users.noreply.github.com>
2023-09-14 14:42:28 +0530

    #include<stdio.h> int main() {     int l,b,w,c,tc,area;     scanf("%d%d%d%d",&l,&b,&w,&c);     area=((l+2*w)*(b+2*w)-(l*b));     tc=c*area;     printf("%d",tc);     }

--- Cost_to_fence_the_park_outside.c
+++ Cost_to_fence_the_park_outside.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-int main()
+int main() 
 {
     int l,b,w,c,tc,area;
     scanf("%d%d%d%d",&l,&b,&w,&c);
     area=((l+2*w)*(b+2*w)-(l*b));
     tc=c*area;
     printf("%d",tc);
-}
+    }
